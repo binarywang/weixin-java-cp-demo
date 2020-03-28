8becb7387272272ff3dc7fe1b39dc3d29931f7d4
Binary Wang <binarywang@gmail.com>
2020-03-28 21:09:24 +0800

    :art: 增加点代码

--- src/main/java/com/github/binarywang/demo/wx/cp/handler/MsgHandler.java
+++ src/main/java/com/github/binarywang/demo/wx/cp/handler/MsgHandler.java
@@ -1,9 +1,5 @@
 package com.github.binarywang.demo.wx.cp.handler;
 
-import java.util.Map;
-
-import org.springframework.stereotype.Component;
-
 import com.github.binarywang.demo.wx.cp.builder.TextBuilder;
 import com.github.binarywang.demo.wx.cp.utils.JsonUtils;
 import me.chanjar.weixin.common.api.WxConsts;
@@ -11,6 +7,9 @@ import me.chanjar.weixin.common.session.WxSessionManager;
 import me.chanjar.weixin.cp.api.WxCpService;
 import me.chanjar.weixin.cp.bean.WxCpXmlMessage;
 import me.chanjar.weixin.cp.bean.WxCpXmlOutMessage;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
 
 /**
  * @author Binary Wang(https://github.com/binarywang)
@@ -18,19 +17,23 @@ import me.chanjar.weixin.cp.bean.WxCpXmlOutMessage;
 @Component
 public class MsgHandler extends AbstractHandler {
 
-  @Override
-  public WxCpXmlOutMessage handle(WxCpXmlMessage wxMessage, Map<String, Object> context, WxCpService cpService,
-                                  WxSessionManager sessionManager) {
+    @Override
+    public WxCpXmlOutMessage handle(WxCpXmlMessage wxMessage, Map<String, Object> context, WxCpService cpService,
+                                    WxSessionManager sessionManager) {
+        final String msgType = wxMessage.getMsgType();
+        if (msgType == null) {
+            // 如果msgType没有，就自己根据具体报文内容做处理
+        }
 
-    if (!wxMessage.getMsgType().equals(WxConsts.XmlMsgType.EVENT)) {
-      //TODO 可以选择将消息保存到本地
+        if (!msgType.equals(WxConsts.XmlMsgType.EVENT)) {
+            //TODO 可以选择将消息保存到本地
+        }
+
+        //TODO 组装回复消息
+        String content = "收到信息内容：" + JsonUtils.toJson(wxMessage);
+
+        return new TextBuilder().build(content, wxMessage, cpService);
+
     }
-
-    //TODO 组装回复消息
-    String content = "收到信息内容：" + JsonUtils.toJson(wxMessage);
-
-    return new TextBuilder().build(content, wxMessage, cpService);
-
-  }
 
 }
