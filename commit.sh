0c18338ef3bd12b55ad3a0daf5998165709c8948
Binary Wang <a@binarywang.com>
2025-01-03 20:37:55 +0800

    update wxjava sdk to 4.7.0

--- commit.sh
+++ commit.sh
@@ -1,5 +1,4 @@
-git commit -a -m "update logger related code , using @slf4j instead"
-#git commit -a -m "update wxjava sdk to 4.6.0"
+git commit -a -m "update wxjava sdk to 4.7.0"
 git pull --rebase
 
 git push origin master
